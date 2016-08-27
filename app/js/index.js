810a38f141702d0666df5c1c2f471c530487f02f
gan <ganmingying@wondersgroup.com>
2016-08-27 23:29:01 +0800

    come on

---
+++ app/js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by GAN on 2016/8/27.
+ */
+var app = angular.module('item', ['ngRoute'], function($routeProvider){
+    $routeProvider
+        .when('/api', {
+        templateUrl: 'html/api.html',
+        controller:'apiController'
+    }).when('/dynamic', {
+        templateUrl: 'html/dynamic.html',
+        controller:'dynamicController'
+    });
+});
+app.controller('itemController',function($scope){
+    $scope.toLogin = function(){
+        location.href = "login.html";
+    }
+});
